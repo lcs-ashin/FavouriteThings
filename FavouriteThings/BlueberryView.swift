ba281bd6204b490f9e3f055f8b24a841fed70022
lcs-ashin <ashin@lcs.on.ca>
2021-10-25 09:03:11 -0400

    Added three pages with images of my favourite things.

--- FavouriteThings/BlueberryView.swift
+++ FavouriteThings/BlueberryView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  BlueberryView.swift
 //  FavouriteThings
 //
 //  Created by Eunbi Shin on 2021-10-21.
@@ -7,16 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct BlueberryView: View {
+    
+    // "body" is a computed property
     var body: some View {
-        Image("Bjorn")
+        return Image("BlueberryPoundCake")
             .resizable()
             .scaledToFit()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct BlueberryView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        BlueberryView()
     }
 }
--- FavouriteThings/ChocolateView.swift
+++ FavouriteThings/ChocolateView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  ChocolateView.swift
 //  FavouriteThings
 //
 //  Created by Eunbi Shin on 2021-10-21.
@@ -7,16 +7,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct ChocolateView: View {
     var body: some View {
-        Image("Bjorn")
+        Image("cherry-garcia")
             .resizable()
             .scaledToFit()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct ChocolateView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ChocolateView()
     }
 }
