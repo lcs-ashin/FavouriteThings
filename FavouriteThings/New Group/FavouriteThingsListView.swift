d207927e587c77c7600e9894796e33c5c8a6aaf0
lcs-ashin <ashin@lcs.on.ca>
2021-10-26 11:30:21 -0400

    Added a list to serve as the landing page (first thing users sees in our app.)

---
+++ FavouriteThings/New Group/FavouriteThingsListView.swift
@@ -0,0 +1,27 @@
+//
+//  FavouriteThingsListView.swift
+//  FavouriteThings
+//
+//  Created by Eunbi Shin on 2021-10-26.
+//
+
+import SwiftUI
+
+struct FavouriteThingsListView: View {
+    var body: some View {
+        List {
+            Text("Bjorn🐶")
+            Text("Icecream🍦")
+            Text("Cake🍰")
+        }
+        .navigationTitle("Favourite Things App")
+    }
+}
+
+struct FavouriteThingsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            FavouriteThingsListView()
+        }
+    }
+}
