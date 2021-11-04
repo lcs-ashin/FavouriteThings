91c94d0b8ca1644c3e2a6596d25dbc1042b55f51
lcs-ashin <ashin@lcs.on.ca>
2021-11-04 14:32:06 -0400

    Extracted the new caption and credit layout into its own view

--- FavouriteThings/ContentView.swift
+++ FavouriteThings/ContentView.swift
@@ -13,19 +13,8 @@ struct ContentView: View {
         ScrollView {
             VStack(alignment: .leading) {
                 
-                VStack(alignment: .leading) {
-                    
-                    Image("Bjorn")
-                        .resizable()
-                        .scaledToFit()
-                  
-                    Group {
-                        Text("A photo of Bjorn.")
-                        Text("Photo credit: Amy Shin")
-                    }
-                    .padding(.horizontal)
-                    .font(.caption)
-                }
+               // Create an INSTANCE of the extracted view
+                ExtractedView()
                 
                 Text("Bjorn is an engergetic puppy. He loves to play and take a nap.🐶 He is one year old so he is curious of everything around him. However, this puppy is afraid of other dogs.😅 He is not quite ready to see the big world.")
                     .padding()
@@ -41,6 +30,25 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
             ContentView()
+        }
+    }
+}
+
+// A new structure has been created from the layout we just extracted
+struct ExtractedView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            
+            Image("Bjorn")
+                .resizable()
+                .scaledToFit()
+            
+            Group {
+                Text("A photo of Bjorn.")
+                Text("Photo credit: Amy Shin")
+            }
+            .padding(.horizontal)
+            .font(.caption)
         }
     }
 }
