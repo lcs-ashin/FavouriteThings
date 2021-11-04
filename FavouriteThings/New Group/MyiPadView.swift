df4680617419c15ccf6c9a1f896518540caed987
lcs-ashin <ashin@lcs.on.ca>
2021-11-04 14:46:37 -0400

    Added PhotoCaptionView to MyIpadView

--- FavouriteThings/New Group/MyiPadView.swift
+++ FavouriteThings/New Group/MyiPadView.swift
@@ -11,9 +11,8 @@ struct MyiPadView: View {
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
-                Image("MyiPad")
-                    .resizable()
-                    .scaledToFit()
+                
+                PhotoCaptionView()
                 
                 Text("This iPad is one of my favourite things because I use it a lot for both entertaining and studing. I like to watch movies and videos on my iPad but I don't have wifi for it on campus so I can't really watch movies. Instead, I love to draw many things on it. Time passes really fast when I draw. Also, I make some notes on iPad and it is very useful!✍️")
                     .padding()
